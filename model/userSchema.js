9ca604908bc9159f02d7b0517802732db4a40b62
Sourabh <sourabhkhot65@gmail.com>
2023-10-29 23:04:06 +0530

    Sigin with JWT and Cookies

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-  email:{
+  email: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
@@ -17,15 +19,42 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  address:{
+  address: {
     type: String,
-    required: true
+    required: true,
   },
   mobileNo: {
     type: Number,
     required: true,
-  }
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+//for hashing the password
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = hashedPass = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const User = mongoose.model("USER", userSchema);
 
--- router/auth.js
+++ router/auth.js
@@ -1,28 +1,29 @@
 const express = require("express");
-const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
 require("../db/conn");
 const User = require("../model/userSchema");
 
 router.get("/", (req, res) => {
   res.send("Hello from server");
 });
-var hashedPass = "";
+
+//Registration route
 router.post("/register", async (req, res) => {
     const { name, email, password, storeName, address, mobileNo } = req.body;
     if (!name || !email || !password || !storeName || !address || !mobileNo) {
         return res.status(422).json({ error: "All fiels are required!" });
     }
     try {
-        hashedPass = await bcrypt.hash(password, 10);
-        // console.log(hashedPass);
         const userExist = await User.findOne({ email:email });
 
         if(userExist){
             return res.status(422).json({error : "E-Mail already registered!"});
         }
 
-        const user = new User({name:name, email:email, password:hashedPass, storeName:storeName, address:address, mobileNo:mobileNo});
+        const user = new User({ name, email, password, storeName, address, mobileNo });
 
         const userRegistered = await user.save();
 
@@ -30,6 +31,40 @@ router.post("/register", async (req, res) => {
             res.status(201).json({message: "Registration Succesfull!"})
         }else{
             res.status(500).json({error:"Registaration Failed!"});
+        }
+    }catch(err){
+        console.log(err);
+    }
+});
+
+//Signin route 
+router.post('/signin', async (req, res) => {
+    console.log(req.body);
+    try{
+        const { email, password } = req.body;
+        
+        if(!email || !password){
+            return res.status(400).json({message : "Email and password Required!"});
+        }
+        const userLogin = await User.findOne({ email:email});
+
+        if(userLogin){
+            const isMatch = await bcrypt.compare(password, userLogin.password);
+
+            const token = await userLogin.generateAuthToken();
+            console.log(token);
+
+            res.cookie("jwtoken", token, {
+                expires: new Date(Date.now() + 3600000),
+                httpOnly: true
+            });
+            if(!isMatch){
+                res.json({error: "Invalid Credentials"});
+            }else{
+                res.json({message: "user signin succesfull!"});
+            }
+        }else{
+            res.json({error: "User Not Found"});
         }
     }catch(err){
         console.log(err);
